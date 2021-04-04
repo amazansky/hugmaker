4647371c25d665b754222479de953d220cb5e047
Alex Mazansky <alex@amazansky.com>
2021-04-04 01:22:41 -0400

    extract flags to separate directory, add generation script

--- main.py
+++ main.py
@@ -11,29 +11,28 @@ with open('config.yml', 'r') as f:
 
 token = config['BOT_TOKEN']
 
-# flag colors use BGR (not RGB!) for consistency with OpenCV
-flagdict = {
-    'abrosexual': [(149, 202, 119), (202, 228, 181), (255, 255, 255), (181, 150, 230), (110, 70, 216)],
-    'asexual': [(0, 0, 0), (163, 163, 163), (255, 255, 255), (128, 0, 128)],
-    'agender': [(0, 0, 0), (185, 185, 185), (255, 255, 255), (131, 244, 184), (255, 255, 255), (185, 185, 185), (0, 0, 0)],
-    'aromantic': [(66, 165, 61), (121, 211, 167), (255, 255, 255), (169, 169, 169), (0, 0, 0)],
-    'bigender': [(157, 122, 193), (200, 165, 234), (229, 198, 212), (255, 255, 255), (229, 198, 212), (230, 198, 155), (203, 130, 107)],
-    'bisexual': [(112, 2, 214), (112, 2, 214), (150, 79, 155), (168, 56, 0), (168, 56, 0)],
-    'demiboy': [(127, 127, 127), (195, 195, 195), (233, 217, 154), (255, 255, 255), (233, 217, 154), (195, 195, 195), (127, 127, 127)],
-    'demigirl': [(127, 127, 127), (195, 195, 195), (200, 175, 254), (255, 255, 255), (200, 175, 254), (195, 195, 195), (127, 127, 127)],
-    'gay': [(113, 141, 19), (171, 206, 45), (195, 232, 153), (255, 255, 255), (223, 173, 124), (199, 73, 79), (119, 28, 61)],
-    'genderfluid': [(162, 117, 255), (255, 255, 255), (214, 24, 190), (0, 0, 0), (189, 62, 51)],
-    'genderqueer': [(220, 126, 181), (255, 255, 255), (35, 129, 74)],
-    'graysexual': [(143, 12, 115), (169, 176, 173), (255, 255, 255), (169, 176, 173), (143, 12, 115)],
-    'grayromantic': [(37, 125, 17), (175, 178, 176), (255, 255, 255), (175, 178, 176), (37, 125, 17)],
-    'lesbian': [(0, 45, 213), (86, 154, 255), (255, 255, 255), (164, 98, 211), (83, 2, 138)],
-    'nonbinary': [(48, 244, 255), (255, 255, 255), (209, 89, 156), (0, 0, 0)],
-    'omnisexual': [(201, 153, 253), (185, 85, 254), (69, 3, 40), (248, 94, 100), (250, 163, 138)],
-    'pansexual': [(140, 33, 255), (0, 216, 255), (255, 177, 33)],
-    'pangender': [(159, 247, 255), (206, 221, 255), (250, 235, 255), (255, 255, 255), (250, 235, 255), (206, 221, 255), (159, 247, 255)],
-    'polysexual': [(185, 28, 246), (105, 213, 7), (246, 146, 28)],
-    'pride': [(3, 3, 228), (0, 140, 255), (0, 237, 255), (38, 128, 0), (255, 77, 0), (135, 7, 117)],
-    'transgender': [(250, 206, 91), (184, 169, 245), (255, 255, 255), (184, 169, 245), (250, 206, 91)]
+flagset = {
+    'abrosexual',
+    'asexual',
+    'agender',
+    'aromantic',
+    'bigender',
+    'bisexual',
+    'demiboy',
+    'demigirl',
+    'gay',
+    'genderfluid',
+    'genderqueer',
+    'graysexual',
+    'grayromantic',
+    'lesbian',
+    'nonbinary',
+    'omnisexual',
+    'pansexual',
+    'pangender',
+    'polysexual',
+    'pride',
+    'transgender'
 }
 
 aliases = {
@@ -92,25 +91,17 @@ async def hug(ctx, p1, p2):
         return cv.warpAffine(img, matrix, dimensions)
 
     # create each flag from its corresponding colors
-    flag1 = np.zeros((img.shape[0], img.shape[1], 4), dtype='uint8')
-    colors1 = flagdict[p1]
-    for i, color in enumerate(colors1):
-        # darken left person if the flags are the same
-        if p1 == p2:
-            color = [int(c*0.8) for c in color]
-        else:
-            color = list(color)
-        color.append(255)
-        color = tuple(color)
-        cv.rectangle(flag1, (0, i*img.shape[1] // len(colors1)), (img.shape[1], (i+1) * img.shape[1] // len(colors1)), color, -1)
+    if p1 in flagset:
+        flag1 = cv.imread(f'flags/{p1}.png', cv.IMREAD_UNCHANGED)
+        flag1 = cv.resize(flag1, (img.shape[0], img.shape[1]))
 
-    flag2 = np.zeros((img.shape[0], img.shape[1], 4), dtype='uint8')
-    colors2 = flagdict[p2]
-    for i, color in enumerate(colors2):
-        color = list(color)
-        color.append(255)
-        color = tuple(color)
-        cv.rectangle(flag2, (0, i*img.shape[1] // len(colors2)), (img.shape[1], (i+1) * img.shape[1] // len(colors2)), color, -1)
+        if p1 == p2:
+            flag1 = flag1 * 0.8
+            flag1 = flag1.astype('uint8')
+
+    if p2 in flagset:
+        flag2 = cv.imread(f'flags/{p2}.png', cv.IMREAD_UNCHANGED)
+        flag2 = cv.resize(flag2, (img.shape[0], img.shape[1]))
     flag2 = rotate(flag2, 5, 1.1)
 
     # use the people as masks for the flags
